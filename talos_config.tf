d7bb32143623c00588863d84a2a36216c3ba230e
Pascal Töpke <pascal.toepke@gmail.com>
2025-01-16 10:37:27 +0100

    beautify comments

--- talos_config.tf
+++ talos_config.tf
@@ -1,30 +1,30 @@
-// Creating secrets for Talos machines (controlplane and worker)
+# Creating secrets for Talos machines (controlplane and worker)
 resource "talos_machine_secrets" "this" {}
 
-// Talos machine configuration for controlplane nodes
+# Talos machine configuration for controlplane nodes
 data "talos_machine_configuration" "controlplane" {
   cluster_name     = var.cluster_name
-  cluster_endpoint = "https://${cidrhost(var.network.cidr, 10)}:6443" //cidrhost(x, 10) equals first controlplane
+  cluster_endpoint = "https:#${cidrhost(var.network.cidr, 10)}:6443" #cidrhost(x, 10) equals first controlplane
   machine_type     = "controlplane"
   machine_secrets  = talos_machine_secrets.this.machine_secrets
 }
 
-// Talos machine configuration for worker nodes
+# Talos machine configuration for worker nodes
 data "talos_machine_configuration" "worker" {
   cluster_name     = var.cluster_name
-  cluster_endpoint = "https://${cidrhost(var.network.cidr, 10)}:6443" //cidrhost(x, 10) equals first controlplane
+  cluster_endpoint = "https:#${cidrhost(var.network.cidr, 10)}:6443" #cidrhost(x, 10) equals first controlplane
   machine_type     = "worker"
   machine_secrets  = talos_machine_secrets.this.machine_secrets
 }
 
-// Talos client configuration for the Kubernetes cluster
+# Talos client configuration for the Kubernetes cluster
 data "talos_client_configuration" "this" {
   cluster_name         = var.cluster_name
   client_configuration = talos_machine_secrets.this.client_configuration
   endpoints            = [for _, controlplane in local.controlplanes : controlplane.ip_address]
 }
 
-// Apply Talos machine configuration to controlplane VMs
+# Apply Talos machine configuration to controlplane VMs
 resource "talos_machine_configuration_apply" "controlplane" {
   depends_on = [proxmox_virtual_environment_vm.controlplane]
   for_each   = local.controlplanes
@@ -41,7 +41,7 @@ resource "talos_machine_configuration_apply" "controlplane" {
   ]
 }
 
-// Apply Talos machine configuration to worker VMs
+# Apply Talos machine configuration to worker VMs
 resource "talos_machine_configuration_apply" "worker" {
   depends_on = [proxmox_virtual_environment_vm.worker]
   for_each   = local.workers
@@ -57,7 +57,7 @@ resource "talos_machine_configuration_apply" "worker" {
   ]
 }
 
-// Bootstrap Talos on the first controlplane node to initialize the Talos cluster
+# Bootstrap Talos on the first controlplane node to initialize the Talos cluster
 resource "talos_machine_bootstrap" "this" {
   depends_on = [talos_machine_configuration_apply.controlplane]
 
@@ -66,7 +66,7 @@ resource "talos_machine_bootstrap" "this" {
   endpoint             = lookup(local.controlplanes[keys(local.controlplanes)[0]], "ip_address")
 }
 
-// Retrieve the Kubernetes kubeconfig
+# Retrieve the Kubernetes kubeconfig
 resource "talos_cluster_kubeconfig" "this" {
   depends_on = [talos_machine_bootstrap.this]
 
