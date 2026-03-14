a081929a22489b9d3776311d277dbaf3543866df
Pascal T. <pascal@toepke.dev>
2026-03-14 21:38:30 +0100

    feat: add kubectl provider for Cilium deployment via helm_template + kubectl_manifest
    Use alekc/kubectl with apply_only to avoid plan-time cluster validation

--- cilium.tf
+++ cilium.tf
@@ -43,17 +43,26 @@ locals {
   YAML
 }
 
-resource "helm_release" "cilium" {
-  count            = var.cilium.enabled ? 1 : 0
-  name             = "cilium"
-  repository       = "https://helm.cilium.io"
-  chart            = "cilium"
-  version          = var.cilium.version
-  namespace        = "kube-system"
-  cleanup_on_fail  = true
-  create_namespace = false
+data "helm_template" "cilium" {
+  count     = var.cilium.enabled ? 1 : 0
+  name      = "cilium"
+  namespace = "kube-system"
+
+  repository = "https://helm.cilium.io"
+  chart      = "cilium"
+  version    = var.cilium.version
 
   values = var.cilium.values != null ? var.cilium.values : [local.cilium_default_values]
+}
 
+data "kubectl_file_documents" "cilium" {
+  count   = var.cilium.enabled ? 1 : 0
+  content = data.helm_template.cilium[0].manifest
+}
+
+resource "kubectl_manifest" "cilium" {
+  for_each   = var.cilium.enabled ? data.kubectl_file_documents.cilium[0].manifests : {}
+  yaml_body  = each.value
+  apply_only = true
   depends_on = [data.http.talos_health]
 }
--- providers.tf
+++ providers.tf
@@ -17,5 +17,9 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 2.0.0"
     }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = ">= 2.1.0"
+    }
   }
 }
