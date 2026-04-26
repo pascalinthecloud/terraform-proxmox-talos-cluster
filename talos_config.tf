a393d9d83336cfc65cbb8e58235375487ad153f6
Pascal T. <pascal@toepke.dev>
2026-04-26 18:08:48 +0200

    fix: remove HostnameConfig from network patch to avoid conflict with base config

--- talos_config.tf
+++ talos_config.tf
@@ -40,7 +40,6 @@ resource "talos_machine_configuration_apply" "controlplane" {
       install_disk = each.value.install_disk
     }),
     templatefile("${path.module}/templates/network-config.yaml.tmpl", {
-      hostname    = each.key
       ip_address  = each.value.ip_address
       subnet      = each.value.subnet
       gateway     = var.network.gateway
@@ -71,7 +70,6 @@ resource "talos_machine_configuration_apply" "worker" {
       install_disk = each.value.install_disk
     }),
     templatefile("${path.module}/templates/network-config.yaml.tmpl", {
-      hostname    = each.key
       ip_address  = each.value.ip_address
       subnet      = each.value.subnet
       gateway     = var.network.gateway
